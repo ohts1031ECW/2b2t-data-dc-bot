e28b153574e170aa0166fd1c571998c56186aaa6
ohts1031ECW <ohashitamc_main@outlook.jp>
2025-02-19 18:40:08 +0900

    add ephemeral option

--- src/Commands/ping.ts
+++ src/Commands/ping.ts
@@ -1,22 +1,32 @@
-import { Message, ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import { Message, ChatInputCommandInteraction, SlashCommandBuilder, SlashCommandBooleanOption, SlashCommandSubcommandBuilder } from "discord.js";
 import { CommandType } from "../types/types";
 
-export const Command:CommandType = {
+export const Command: CommandType = {
     data: new SlashCommandBuilder()
-    .setName("ping")
-    .setDescription("get ping"),
+        .setName("ping")
+        .setDescription("get ping")
+        .addBooleanOption(option =>
+            option.setName("ephemeral")
+                .setDescription("is visible")
+                .setRequired(false))
+    ,
 
-    execute: async(command)=>{
+    execute: async (command) => {
 
         //type 0 Message
-        if(command.type === 0){
-            const message:Message = command;
+        if (command.type === 0) {
+            const message: Message = command;
             message.reply(`pong!\n ${message.client.ws.ping} ms`)
 
             //type 2 ChatInputCommandInteraction
-        } else if(command.type === 2){
-            const interaction:ChatInputCommandInteraction= command;
-            interaction.reply({content: `pong!\n ${interaction.client.ws.ping}ms`,ephemeral:true})
-        } 
+        } else if (command.type === 2) {
+            const interaction: ChatInputCommandInteraction = command;
+            const ephemeral: boolean = interaction.options.getBoolean("ephemeral") || false;
+
+            interaction.reply({
+                content: `pong!\n ${interaction.client.ws.ping}ms`,
+                ephemeral: ephemeral
+            })
+        }
     }
 }
